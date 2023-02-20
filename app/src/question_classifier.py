b71a98d2ec13f4e914e12619f4b2cccb4f656eca
balendra-singh <balendrasingh19@hotmail.com>
2023-02-20 19:00:59 +0000

    added glove

--- app/src/question_classifier.py
+++ app/src/question_classifier.py
@@ -163,3 +163,42 @@ embedded = embedding(inputs)
 
 print(embedded.shape)
 
+
+
+
+
+
+
+# GloVe
+glove_path = './app/data/glove.6B.50d.txt'
+vocab,embeddings = [],[]
+with open(glove_path,'rt', encoding='utf-8') as fi:
+    full_content = fi.read().strip().split('\n')
+for i in range(len(full_content)):
+    i_word = full_content[i].split(' ')[0]
+    i_embeddings = [float(val) for val in full_content[i].split(' ')[1:]]
+    vocab.append(i_word)
+    embeddings.append(i_embeddings)
+
+
+vocab_npa = np.array(vocab)
+embs_npa = np.array(embeddings)
+
+#insert '<pad>' and '<unk>' tokens at start of vocab_npa.
+vocab_npa = np.insert(vocab_npa, 0, '<pad>')
+vocab_npa = np.insert(vocab_npa, 1, '<unk>')
+print(vocab_npa[:10])
+
+pad_emb_npa = np.zeros((1,embs_npa.shape[1]))   #embedding for '<pad>' token.
+unk_emb_npa = np.mean(embs_npa,axis=0,keepdims=True)    #embedding for '<unk>' token.
+
+#insert embeddings for pad and unk tokens at top of embs_npa.
+embs_npa = np.vstack((pad_emb_npa,unk_emb_npa,embs_npa))
+print(embs_npa.shape)
+
+my_embedding_layer = torch.nn.Embedding.from_pretrained(torch.from_numpy(embs_npa).float())
+
+assert my_embedding_layer.weight.shape == embs_npa.shape
+print(my_embedding_layer.weight.shape)
+
+
